44f236e18ba8ede8e0ba02aa1ef58ba2e52ac489
Trần Nhật Duy <contact@duydev.me>
2017-03-21 23:38:23 +0700

    @Add some widget Dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Avatar;
+use App\Frame;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -9,6 +11,8 @@ class DashboardController extends Controller
 {
     public function index(){
         $page_title = 'Bảng Điều Khiển';
-        return view( 'admin.dashboard', compact( 'page_title' ) );
+        $photo_created = Avatar::count();
+        $frame_uploaded = Frame::count();
+        return view( 'admin.dashboard', compact( 'page_title', 'photo_created', 'frame_uploaded' ) );
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,4 +1,34 @@
 @extends( 'admin.layout' )
 
 @section( 'content' )
+    <div class="row">
+        <div class="col-md-6 col-sm-6 col-xs-12">
+            <div class="info-box">
+                <span class="info-box-icon bg-green"><i class="fa fa-file-image-o"></i></span>
+
+                <div class="info-box-content">
+                    <span class="info-box-text">Số Avatar Được Tạo</span>
+                    <span class="info-box-number">{{ $photo_created }} <small>ảnh</small></span>
+                </div>
+                <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+        <div class="col-md-6 col-sm-6 col-xs-12">
+            <div class="info-box">
+                <span class="info-box-icon bg-blue"><i class="fa fa-photo"></i></span>
+
+                <div class="info-box-content">
+                    <span class="info-box-text">Số Khung Được Tải Lên</span>
+                    <span class="info-box-number">{{ $frame_uploaded }} <small>khung</small></span>
+                </div>
+                <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+        <!-- fix for small devices only -->
+        <div class="clearfix visible-sm-block"></div>
+    </div>
 @endsection
